a59b7e0d9574335e9e151693be338af02094a387
Wesley <wesleyandrade298@gmail.com>
2021-01-04 22:44:32 -0300

    Domain Model

---
+++ src/main/java/com/dsdeliver/entities/Product.java
@@ -0,0 +1,44 @@
+package com.dsdeliver.entities;
+
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name="tb_product")
+public class Product implements Serializable {
+	
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@EqualsAndHashCode.Include
+	private Long id;
+	
+	private String name;
+	private Double price;
+	private String description;
+	private String imageUri;
+	
+	
+	
+	
+
+}

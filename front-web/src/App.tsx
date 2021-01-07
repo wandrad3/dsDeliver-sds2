c4696a30aa908c45141b3ce6ded1a2d153f85cf1
Wesley <wesleyandrade298@gmail.com>
2021-01-06 23:49:56 -0300

    Add NavBar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Navbar from './NavBar';
 
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      <Navbar />
     </div>
   );
 }

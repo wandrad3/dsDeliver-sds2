0c949e680e1a6e89f8ff1d7c7b8ed16965a5a424
Wesley <wesleyandrade298@gmail.com>
2021-01-07 21:26:38 -0300

    Add Home Page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
+import Home from './Home';
 import Navbar from './NavBar';
 
 function App() {
   return (
+  <>
     <div>
       <Navbar />
+      <Home />
     </div>
+  </>
   );
 }
 

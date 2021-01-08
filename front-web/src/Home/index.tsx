2eb2aada0ff578cb33da17b2e363e99815c9b4d7
Wesley <wesleyandrade298@gmail.com>
2021-01-07 22:01:13 -0300

    Add Footer

--- front-web/src/Home/index.tsx
+++ front-web/src/Home/index.tsx
@@ -1,31 +1,35 @@
 import './styles.css';
-import {ReactComponent as MainImage} from './main.svg'
+import { ReactComponent as MainImage } from './main.svg'
+import Footer from '../Footer';
 
 
-function Home(){
-    return(
-        <div className="home-container">
-            <div className="home-content">
-                <div className="home-action">
-                    <h1 className="home-title">
-                        Faça seu pedido <br/>
-                        que entregamos <br/>
+function Home() {
+    return (
+        <>
+            <div className="home-container">
+                <div className="home-content">
+                    <div className="home-action">
+                        <h1 className="home-title">
+                            Faça seu pedido <br />
+                        que entregamos <br />
                         pra você!!!
                     </h1>
-                    <h3 className="home-subtitle">
-                        Escolha seu pedidos em poucos minutos <br/> levaremos na sua porta
+                        <h3 className="home-subtitle">
+                            Escolha seu pedidos em poucos minutos <br /> levaremos na sua porta
                     </h3>
-                    <a href="order" className="home-btn-order">
-                        FAZER PEDIDO
+                        <a href="order" className="home-btn-order">
+                            FAZER PEDIDO
                     </a>
-                    
-                </div>
-                <div className="home-image">
-                    <MainImage/>
+
+                    </div>
+                    <div className="home-image">
+                        <MainImage />
+                    </div>
                 </div>
             </div>
-        </div>
-        
+            <Footer />
+        </>
+
     )
 
 }

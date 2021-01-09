e37dc2652cdabd038a5b625eed3f90b7e6dc6509
Wesley <wesleyandrade298@gmail.com>
2021-01-09 17:21:35 -0300

    Add env vars

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { OrderPayLoad } from "./Orders/Types";
-const API_URL= 'http://localhost:8080'
+const API_URL= process.env.REACT_APP_API_URL;
 const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
 
 export function fetchProducts(){
